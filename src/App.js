7c630db8ece132616c31c336e0db5f1079927e2f
Mayinja6 <douglasmayinja6@gmail.com>
2022-04-29 20:24:27 +0300

    Halfway shortenin component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { Cta, Footer } from "./components";
+import { Shortenin, Cta, Footer } from "./components";
 
 function App() {
   return (
     <div>
+      <Shortenin />
       <Cta />
       <Footer />
     </div>
--- src/components/shortenin/Shortenin.jsx
+++ src/components/shortenin/Shortenin.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
 import "./shortenin.css";
 
 const Shortenin = () => {
-  return <div>Shortenin</div>;
+  const [url, setUrl] = useState("");
+
+  return (
+    <div className="shortenin">
+      <div className="shortenin_calltoaction">
+        <input
+          type="url"
+          placeholder="Shorten a link here..."
+          className="shortenin_calltoaction-input"
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <button className="shortenin_calltoaction-btn cyan_btn">
+          Shorten it!
+        </button>
+      </div>
+      <div className="shortenin_results">Shortenin Results</div>
+    </div>
+  );
 };
 
 export default Shortenin;
