048b1fa586f8e0473c6fcedc72b7845510b6cda9
Mayinja6 <douglasmayinja6@gmail.com>
2022-05-02 11:12:02 +0300

    navbar responsiveness

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,22 @@
 import "./header.css";
-
+import { useState } from "react";
 import { icon_humburger, Logo } from "../../assets";
 
 const Header = () => {
+  const [navigationMenu, setNavigationMenu] = useState(false);
+
   return (
     <div className="header" id="footer">
       <div className="header_logo">
         <img src={Logo} alt="Website Logo" />
       </div>
-      <div className="header_content">
+      <div className={`header_content ${navigationMenu ? "toggled" : ""}`}>
         <div className="header_content_navigation">
           <p>Features</p>
           <p>Pricing</p>
           <p>Resources</p>
         </div>
+        <hr className="header_content-hr" />
         <div className="header_content_buttons">
           <button type="button" className="header_content_buttons-signin">
             Login
@@ -27,7 +30,11 @@ const Header = () => {
         </div>
       </div>
       <div className="header_menutoggler">
-        <img src={icon_humburger} alt="Menu Toggler" />
+        <img
+          src={icon_humburger}
+          alt="Menu Toggler"
+          onClick={() => setNavigationMenu(!navigationMenu)}
+        />
       </div>
     </div>
   );
